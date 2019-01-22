b3b6993e31a5f9093bed3ccf97c5f45532f84a1e
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-01-22 15:55:51 -0800

    this is my final commit for Javascript-II

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -79,11 +79,11 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce((acc,item)=>{
-    return acc + item.donation;
-}, 0);
-
-console.log(ticketPriceTotal);
+runners.forEach(function (runner) {
+    ticketPriceTotal.push(runner["donation"]);
+});
+const reducer = (accum, currentVal) => accum + currentVal;
+console.log(ticketPriceTotal.reduce(reducer));
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const favoritesandwich = () => {
+  let food = " is my favorite item in a sandwich"
+  return {
+    Ham: () => { return "Ham" + food },
+    Turkey: () => { return "Turkey" + food }
+  }
+}
+
+const newFavoritesandwich = favoritesandwich();
+
+console.log(newFavoritesandwich.Ham());
+console.log(newFavoritesandwich.Turkey());
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+let count = 0;
+return function (){
+  return ++count;
+}
 };
+
+const newCounter =counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
